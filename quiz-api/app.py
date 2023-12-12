1fb96c8411177520fd3a33b6efc189880d857234
Hitsuji <erwann.masson@gmail.com>
2023-12-12 16:48:33 +0100

    end setup services

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,15 +1,25 @@
+import sqlite3
+
 from flask import Flask
 from flask_cors import CORS
-
 from services.QuestionsService import *
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def hello_world():
-	x = 'world'
-	return f"Hello, {x}"
+    return "Hello World!"
+
+@app.route('/quiz-info')
+def quiz_info():
+    conn = get_db_connection()
+    return str(QuestionsService.get_questions(conn))
 
 if __name__ == "__main__":
     app.run()
--- quiz-api/services/QuestionsService.py
+++ quiz-api/services/QuestionsService.py
@@ -1,25 +1,23 @@
 from entities.Question import Question
+from sqlite3 import Connection
 
 class QuestionsService : 
 
     @staticmethod
-    def createQuestion(titre, image, content_question):
-        quest = Question(
-            
-        )
+    def create_question(conn: Connection, titre: str, image: str, content_question: str):
         return True
 
+    def get_questions(conn: Connection):
+        return 5
+    
     @staticmethod
-    def getQuestion():
-        
+    def get_question_by_id(conn: Connection, id_question: int):
         return []
-
+    
     @staticmethod
-    def deleteResult(id_result):
-        
+    def delete_results(conn: Connection):
         return True
 
     @staticmethod
-    def deleteAllResults():
-        
+    def delete_result_by_id(conn: Connection, id_result: int):
         return True
