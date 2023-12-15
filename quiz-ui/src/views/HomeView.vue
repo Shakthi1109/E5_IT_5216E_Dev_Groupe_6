e64302db5d91e802089b3982d31ab6f3e9e21d98
Hitsuji <erwann.masson@gmail.com>
2023-12-15 16:09:36 +0100

    added api call test

--- quiz-ui/src/views/HomeView.vue
+++ quiz-ui/src/views/HomeView.vue
@@ -1,10 +1,23 @@
 <script setup>
 // import TheWelcome from '../components/TheWelcome.vue'
-</script>
 
+function greet(event) {
+  const request = new Request("http://127.0.0.1:5000", {
+    method: "GET"
+  });
+
+  fetch(request)
+    .then((response) => {
+      if (response.status === 200) {
+        console.log(response.json())
+      }
+    })
+}
+
+</script>
 
 <template>
   <h1>Quiz App</h1>
   <p>Test your knowledge</p>
-  <button>Start</button>
+  <button @click="greet">Start</button>
 </template>
