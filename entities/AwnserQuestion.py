ebee9fb2be9ad0a63babf0083dd9ef14504325e4
MathieuLsr <94841682+MathieuLsr@users.noreply.github.com>
2024-01-10 22:20:32 +0100

    Delete entities directory

--- entities/AwnserQuestion.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class AwnserQuestion :
-    id: str = None 
-    id_question: str = None 
-    content: str = None
-    is_correct: bool = False
-
--- entities/ResultQuestion.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class ResultQuestion :
-    #UUID Result, UUID Question, UUID AwnserQuestion
-    id: str = None 
-    id_question: str = None
-    id_awnser_question: str = None
-
