dea6bb6b7ac8eea8ebf14afc649d0031f887fb3e
Shakthi Vel <mail2shakthivel98@gmail.com>
2024-01-23 21:19:50 +0100

    Bug fixes home screen

--- quiz-ui/src/views/HomePage.vue
+++ quiz-ui/src/views/HomePage.vue
@@ -1,45 +1,105 @@
 <template>
-  <div>
-    <div class="wrapper-class">
-      <div class="wrapper">
-        
-        <div class="rounded-text">
-          Start the Quiz ! 
+  <div class="wrapper-class1">
+    <div class="wrapper1">
+      <div class="content-container">
+        <div class="rounded-text1">
+          Start the Quiz !
           <p></p>
           <router-link to="/start-new-quiz-page" class="glow-on-hover">
-              <button class="custom-button">Take Quiz</button>
+            <button class="custom-button1">Take Quiz</button>
           </router-link>
         </div>
 
+        <div class="top-scores1">
+          <h4>Top 3 Players</h4>
+          <div class="top-scores-list1">
+            <span v-for="(bestScore, index) in topScores()" :key="index">
+              <span v-if="index === 0">1st</span>
+              <span v-else-if="index === 1">2nd</span>
+              <span v-else-if="index === 2">3rd</span>
+              - {{ bestScore.playerName }}
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 
-<style>
+<script>
+import quizApiService from "@/services/QuizApiService";
 
-h2, h3 {
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      registeredScores: [],
+      scoreColumns: [] // Tableau pour stocker les colonnes de scores
+    };
+  },
+  async created() {
+    var quizInfoPromise = quizApiService.getQuizInfo();
+    var quizInfoAPIResult = await quizInfoPromise;
+    this.registeredScores = quizInfoAPIResult.data.scores;
+    this.updateScoreColumns(); // Appeler la fonction pour mettre à jour les colonnes de scores
+    console.log("Composant Home page 'created'");
+    console.log("Registered Scores :", this.registeredScores);
+  },
+  methods: {
+    topScores() {
+      const topThreeScores = this.registeredScores.slice(0, 3); // Récupérer uniquement les trois premiers scores
+      return topThreeScores.map((score, index) => ({
+        ...score,
+        stars: Array(3-index).fill(0) // Générer un tableau avec le nombre d'étoiles correspondant au score
+      }));
+    },
+    updateScoreColumns() {
+      const columnCount = Math.ceil(this.registeredScores.length / 10); // Calculer le nombre de colonnes nécessaires
+      this.scoreColumns = []; // Réinitialiser le tableau des colonnes
+      for (let i = 0; i < columnCount; i++) {
+        const start = i * 10; // Indice de début de la colonne
+        const end = (i + 1) * 10; // Indice de fin de la colonne
+        const scores = this.registeredScores.slice(start, end); // Extraire les scores pour la colonne actuelle
+        this.scoreColumns.push({ id: i, scores }); // Ajouter la colonne au tableau des colonnes
+      }
+    }
+  }
+};
+</script>
+
+<style>
+h2,
+h3 {
   font-size: 2.5rem;
   font-weight: bold;
   margin-bottom: 1.5rem;
 }
 
-.rounded-text {
-  text-align: center;
-  font-size: 24px;
-  border-radius: 10px;
-  padding: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+h4 {
   color: #ffffff;
 }
 
-
-.custom-button {
+.content-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   position: relative;
+  height: 100vh; /* Use full height of the viewport */
+}
+
+.rounded-text1 {
+  text-align: center;
+  font-size: 24px;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 20px;
+}
+
+.custom-button1 {
   padding: 12px 35px;
   background: #41d215;
   font-size: 17px;
@@ -48,35 +108,48 @@ h2, h3 {
   border: 3px solid #ffffff;
   border-radius: 8px;
   margin-top: 1em;
+  cursor: pointer;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 20px;
 }
 
-
-.custom-button:hover {
+.custom-button1:hover {
   color: #000000;
 }
 
-.custom-button:active {
-  transform: scale(0.95);
+.top-scores1 {
+  text-align: center;
+  font-size: 24px;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  padding: 20px;
 }
 
-.wrapper-class {
-  position: relative;
-  height: 100vh;
-  width: 100%;
-  overflow: hidden;
+.top-scores-list1 {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  font-size: 24px;
+  color: #ffffff;
+  padding: 20px;
 }
 
-.wrapper {
-  perspective: 100px;
-  background-color: #000000;
-  overflow-x: hidden;
-  overflow-y: auto;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+.top-scores-list1 span {
+  display: flex;
+  align-items: center;
+  font-size: 24px;
 }
 
+.top-scores-list1 img {
+  width: 40px;
+  margin-left: 20px;
+}
 
+.top-scores-list1 span:not(:last-child) {
+  margin-right: 10px;
+}
 </style>
--- quiz-ui/src/views/ScoreBoardPage.vue
+++ quiz-ui/src/views/ScoreBoardPage.vue
@@ -1,188 +1,189 @@
 <!-- Shak -->
 <template>
-    <div class="wrapper-class">
-      <div class="wrapper">
-        <div class="score-container">
-            <h2>Score Board</h2>
-            <div class="score-board">
-                <div class="score-column" v-for="column in scoreColumns" v-bind:key="column.id">
-                    <div class="score-entry" v-for="scoreEntry in column.scores">
-                    {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
-                    </div>
-                </div>
-            </div>
-            <div class="top-scores">
-            <h2>Top Scorers</h2>            
-            <div class="top-scores-list" >
-                <span v-for="(bestScore, index) in topScores()">
-                <img v-if="index === 0"/>
-                <img v-else-if="index === 1"/>
-                <img v-else-if="index === 2"/>
-                - {{ bestScore.playerName }}
-                </span>
-            </div>
-            </div>
+  <div class="wrapper-class">
+    <div class="wrapper">
+      <div class="score-container">
+          <h2>Score Board</h2>
+          <div class="score-board">
+              <div class="score-column" v-for="column in scoreColumns" v-bind:key="column.id">
+                  <div class="score-entry" v-for="scoreEntry in column.scores">
+                  {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+                  </div>
+              </div>
+          </div>
+          <div class="top-scores">
+          <h2>Top Scorers</h2>            
+          <div class="top-scores-list">
+          <span v-for="(bestScore, index) in topScores()" :key="index">
+            <span v-if="index === 0">1st</span>
+            <span v-else-if="index === 1">2nd</span>
+            <span v-else-if="index === 2">3rd</span>
+            - {{ bestScore.playerName }}
+          </span>
         </div>
-        </div>
-    </div>
+          </div>
+      </div>
+      </div>
+  </div>
 </template>
-  
+
 <script>
 import quizApiService from "@/services/QuizApiService";
 
 export default {
-  name: "ScoreBoardPage",
-  data() {
-    return {
-      registeredScores: [],
-      scoreColumns: []
-    };
+name: "ScoreBoardPage",
+data() {
+  return {
+    registeredScores: [],
+    scoreColumns: []
+  };
+},
+async created() {
+  var quizInfoPromise = quizApiService.getQuizInfo();
+  var quizInfoAPIResult = await quizInfoPromise;
+  this.registeredScores = quizInfoAPIResult.data.scores;
+  this.updateScoreColumns();
+  console.log("Composant Home page 'created'");
+  console.log("Registered Scores :", this.registeredScores);
+},
+methods: {
+  topScores() {
+    const topThreeScores = this.registeredScores.slice(0, 3);
+    return topThreeScores.map((score, index) => ({
+      ...score,
+      stars: Array(3-index).fill(0) 
+    }));
   },
-  async created() {
-    var quizInfoPromise = quizApiService.getQuizInfo();
-    var quizInfoAPIResult = await quizInfoPromise;
-    this.registeredScores = quizInfoAPIResult.data.scores;
-    this.updateScoreColumns();
-    console.log("Composant Home page 'created'");
-    console.log("Registered Scores :", this.registeredScores);
-  },
-  methods: {
-    topScores() {
-      const topThreeScores = this.registeredScores.slice(0, 3);
-      return topThreeScores.map((score, index) => ({
-        ...score,
-        stars: Array(3-index).fill(0) 
-      }));
-    },
-    updateScoreColumns() {
-      const columnCount = Math.ceil(this.registeredScores.length / 10);
-      this.scoreColumns = []; 
-      for (let i = 0; i < columnCount; i++) {
-        const start = i * 10; 
-        const end = (i + 1) * 10; 
-        const scores = this.registeredScores.slice(start, end); 
-        this.scoreColumns.push({ id: i, scores });
-      }
+  updateScoreColumns() {
+    const columnCount = Math.ceil(this.registeredScores.length / 10);
+    this.scoreColumns = []; 
+    for (let i = 0; i < columnCount; i++) {
+      const start = i * 10; 
+      const end = (i + 1) * 10; 
+      const scores = this.registeredScores.slice(start, end); 
+      this.scoreColumns.push({ id: i, scores });
     }
   }
+}
 };
 </script>
 
 <style>
 
 .wrapper-class {
-  position: relative;
-  height: 100vh;
-  width: 100%;
-  overflow: hidden;
+position: relative;
+height: 100vh;
+width: 100%;
+overflow: hidden;
 }
 
 .wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  perspective: 100px;
-  background-color: #000000;
-  overflow-x: hidden;
-  overflow-y: auto;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+display: flex;
+align-items: center;
+justify-content: center;
+perspective: 100px;
+background-color: #000000;
+overflow-x: hidden;
+overflow-y: auto;
+position: absolute;
+top: 0;
+left: 0;
+right: 0;
+bottom: 0;
 }
 
 
 .score-container {
-  text-align: center;
+text-align: center;
+color: #ffffff;
 }
 .score-board {
-  margin-bottom: 1.5rem;
-  display: flex;
-  flex-wrap: wrap; 
-  justify-content: center;
+margin-bottom: 1.5rem;
+display: flex;
+flex-wrap: wrap; 
+justify-content: center;
 }
 
 .score-column {
-  margin: 0 1rem;
+margin: 0 1rem;
 }
 
 .score-entry {
-  margin-bottom: 0.5rem;
+margin-bottom: 0.5rem;
 }
 
 .top-scores {
-  margin-bottom: 1.5rem;
-  text-align: center;
+margin-bottom: 1.5rem;
+text-align: center;
+color: #ffffff;
 }
 
 .top-scores-list {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+display: flex;
+flex-direction: column;
+align-items: center;
 }
 
 .top-scores-list span {
-  display: flex;
-  align-items: center;
-  font-size: 24px;
+display: flex;
+align-items: center;
+font-size: 24px;
 }
 
 .top-scores-list img {
-  width: 40px; 
-  margin-left: 20px; 
+width: 40px; 
+margin-left: 20px; 
 }
 
 .top-scores-list span:not(:last-child) {
-  margin-right: 10px;
+margin-right: 10px;
 }
 
 
 .rounded-text {
-  text-align: center;
-  font-size: 24px;
-  border-radius: 10px;
-  padding: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: #ffffff;
+text-align: center;
+font-size: 24px;
+border-radius: 10px;
+padding: 20px;
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+color: #ffffff;
 }
 
 .go-btn {
-  position: relative;
-  padding: 10px 35px;
-  background: #41d215;
-  font-size: 17px;
-  font-weight: 500;
-  color: white;
-  border: 1px solid #ffffff;
-  border-radius: 8px;
-  margin-top: 1em;
+position: relative;
+padding: 10px 35px;
+background: #41d215;
+font-size: 17px;
+font-weight: 500;
+color: white;
+border: 1px solid #ffffff;
+border-radius: 8px;
+margin-top: 1em;
 }
 
 .go-btn:hover {
-  color: #000000;
+color: #000000;
 }
 
 
-/* Le bouton */
 .custom-button {
-  position: relative;
-  padding: 12px 35px;
-  background: #41d215;
-  font-size: 17px;
-  font-weight: 500;
-  color: white;
-  border: 3px solid #ffffff;
-  border-radius: 8px;
-  margin-top: 1em;
+position: relative;
+padding: 12px 35px;
+background: #41d215;
+font-size: 17px;
+font-weight: 500;
+color: white;
+border: 3px solid #ffffff;
+border-radius: 8px;
+margin-top: 1em;
 }
 
 
 .custom-button:hover {
-  color: #000000;
+color: #000000;
 }
 
 </style>
